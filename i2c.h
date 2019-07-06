8bf7720e454b0a6f598ab0da588a4a0c7852177e
N Srinivas <striker.dbz@hotmail.com>
2019-07-06 17:03:13 +0530

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -1,9 +1,20 @@
 /*
- * i2c.h
- *
- * Created: 31-05-2015 00:30:02
- *  Author: neutron
- */
+i2c.h - Header file for standard i2c functions for AVR Arduino Library.
+Version: 1.2.0
+Date: 31-May-2015
+(c) 2019 N.Srinivas (a.k.a. neutronstriker / www.xmechatronics.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 
 #ifndef I2C_H_
